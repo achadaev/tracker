8492f390022bc6beba99106de34366e12606f375
achadaev <andrew.chadaev@gmail.com>
2019-12-09 21:51:49 +0300

    removed editing

--- src/main/java/com/example/tracker/client/event/EditExpenseEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.example.tracker.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface EditExpenseEventHandler extends EventHandler {
-    void onEditExpense(EditExpenseEvent event);
-}
